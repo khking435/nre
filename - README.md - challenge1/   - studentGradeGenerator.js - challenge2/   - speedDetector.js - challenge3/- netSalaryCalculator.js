52fef4bec85576ef3dce7d1adcc8664ef43882e4
khking435 <162417437+khking435@users.noreply.github.com>
2024-03-19 15:18:07 +0300

    Update - netSalaryCalculator.js

--- - README.md - challenge1/   - studentGradeGenerator.js - challenge2/   - speedDetector.js - challenge3/- netSalaryCalculator.js
+++ - README.md - challenge1/   - studentGradeGenerator.js - challenge2/   - speedDetector.js - challenge3/- netSalaryCalculator.js
@@ -1,1 +1,20 @@
+// studentGradeGenerator.js
+
+function calculateGrade(mark) {
+    if (mark > 79) {
+        return 'A';
+    } else if (mark >= 60 && mark <= 79) {
+        return 'B';
+    } else if (mark >= 50 && mark <= 59) {
+        return 'C';
+    } else if (mark >= 40 && mark <= 49) {
+        return 'D';
+    } else {
+        return 'E';
+    }
+}
+
+const marks = parseFloat(prompt("Enter student marks: "));
+const grade = calculateGrade(marks);
+console.log(`Grade: ${grade}`);
 
